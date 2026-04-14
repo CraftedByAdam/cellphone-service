e24974ee6dd977ea71cfae92359c373bf413ce10
Your Name <youremail@example.com>
2026-04-14 15:54:12 -0400

    I made the code way easier to look at and saved space from line 18-35.

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -15,43 +15,23 @@ public class CellPhone {
         this.owner = "";
     }
 
-    public int getSerialNumber() {
-        return serialNumber;
-    }
+    public int getSerialNumber() {return serialNumber;}
 
-    public void setSerialNumber(int serialNumber) {
-        this.serialNumber = serialNumber;
-    }
+    public void setSerialNumber(int serialNumber) {this.serialNumber = serialNumber;}
 
-    public String getModel() {
-        return model;
-    }
+    public String getModel() {return model;}
 
-    public void setModel(String model) {
-        this.model = model;
-    }
+    public void setModel(String model) {this.model = model;}
 
-    public String getCarrier() {
-        return carrier;
-    }
+    public String getCarrier() {return carrier;}
 
-    public void setCarrier(String carrier) {
-        this.carrier = carrier;
-    }
+    public void setCarrier(String carrier) {this.carrier = carrier;}
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+    public String getPhoneNumber() {return phoneNumber;}
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
 
-    public String getOwner() {
-        return owner;
-    }
+    public String getOwner() {return owner;}
 
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
+    public void setOwner(String owner) {this.owner = owner;}
 }
